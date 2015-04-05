5941710d32a8d815576d15674ab7ae820d314d02
jdwong-infusion <jdwong@infusion.com>
2015-04-05 19:03:05 -0400

    Initial Commit

---
+++ bootcamp-oo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bootcamp_oo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press Enter to exit...");
+            Console.ReadLine(); // waits for line of input
+        }
+    }
+}
